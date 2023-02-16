580661a5d176dcfd5f530ae323c61238d698b2fb
Lucy Lee <leelucy412@gmail.com>
2023-02-16 11:19:54 -0500

    Allow Express to take in form data

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const taskRoutes = require('./src/routes');
+
 const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true })) // Access form data
 app.use(express.json()); // Parse form data
 
+// Homepage route
 app.get('/', (req, res) => {
   res.send("GET request successful.");
 });
 
+// Tasks route
 app.use('/tasks', taskRoutes);
 
 app.listen(port, () => {
