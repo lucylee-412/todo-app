eb1ceb4f251566fe6882339c744724e9ca8f70fe
Lucy Lee <leelucy412@gmail.com>
2023-02-16 08:32:24 -0500

    Add cors middleware

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const taskRoutes = require('./src/routes');
 const app = express();
 const port = 3000;
+
+app.use(cors());
+app.use(express.json()); // Parse form data
 
 app.get('/', (req, res) => {
   res.send("GET request successful.");
@@ -10,5 +14,5 @@ app.get('/', (req, res) => {
 app.use('/tasks', taskRoutes);
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}.`);
 });
